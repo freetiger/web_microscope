aa70af0f95b526d1148705f42ec65adad28073f9
freetiger <freetiger.cn@gmail.com>
2014-08-25 20:13:32 +0800

    package change

---
+++ src/com/freetiger/microscope/ocr/TestOCR.java
@@ -0,0 +1,22 @@
+package com.freetiger.microscope.ocr;
+import java.io.File;  
+import java.io.IOException;  
+  
+public class TestOCR {  
+  
+    /** 
+     * @param args 
+     */  
+    public static void main(String[] args) {  
+        String path = "d:\\code.jpg";     
+        try {     
+            String valCode = new OCR().recognizeText(new File(path), "jpg", null);     
+            System.out.println(valCode);     
+        } catch (IOException e) {     
+            e.printStackTrace();     
+        } catch (Exception e) {  
+            e.printStackTrace();  
+        }      
+    }  
+  
+}  
